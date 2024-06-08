4a3fcaa9f1269a2269fd8efa518c1e315555a2d6
Adam Mazur <mazut@o2.pl>
2024-06-08 11:27:03 +0200

    finished calculator tests

--- src/test/java/org/shop/invoice/CalculatorClassUnitTests.java
+++ src/test/java/org/shop/invoice/CalculatorClassUnitTests.java
@@ -1,12 +1,9 @@
 package org.shop.invoice;
 
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.shop.entities.Invoice;
 import org.shop.entities.InvoiceItem;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,16 +16,17 @@ public class CalculatorClassUnitTests {
     @Test
     void shouldCalculateAndSetCorrectGrossItemPrice() {
         //GIVEN
-        Set<InvoiceItem> invoiceItems = new HashSet<>();
-        invoiceItems.add(new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, null));
-        invoiceItems.add(new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, null));
-        invoiceItems.add(new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, null));
+        //GIVEN
+        Set<InvoiceItem> invoiceItems = Set.of(
+                new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, null),
+                new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, null),
+                new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, null));
         Invoice invoice = new Invoice(1L, 1, "112/05/2024", 1,
                 null, null, null, null, null, invoiceItems);
-        Set<InvoiceItem> invoiceItemsWithExpectedGrossValue = new HashSet<>();
-        invoiceItemsWithExpectedGrossValue.add(new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, 24.07));
-        invoiceItemsWithExpectedGrossValue.add(new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, 44.42));
-        invoiceItemsWithExpectedGrossValue.add(new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, 0.91));
+        Set<InvoiceItem> invoiceItemsWithExpectedGrossValue = Set.of(
+                new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, 24.07),
+                new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, 44.42),
+                new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, 0.91));
         Invoice expectedInvoice = new Invoice(1L, 1, "112/05/2024", 1,
                 null, null, null, null, null, invoiceItemsWithExpectedGrossValue);
         //WHEN
@@ -37,14 +35,46 @@ public class CalculatorClassUnitTests {
                 .map(InvoiceItem::getGrossValue)
                 .collect(Collectors.toSet());
         Set<Double> setOfExpectedGrossItemValues = expectedInvoice.getInvoiceItems().stream()
-                        .map(InvoiceItem::getGrossValue)
-                                .collect(Collectors.toSet());
+                .map(InvoiceItem::getGrossValue)
+                .collect(Collectors.toSet());
         //THEN
         assertThat(setOfCalculatedGrossItemValues).containsExactlyInAnyOrderElementsOf(setOfExpectedGrossItemValues);
         assertThat(invoiceWithCalculatedInvoiceItems)
                 .usingRecursiveComparison()
                 .isEqualTo(expectedInvoice);
 
+    }
+
+    @Test
+    void shouldCalculateCorrectInvoiceNetAmount() {
+        //GIVEN
+        Set<InvoiceItem> invoiceItems = Set.of(
+                new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, 24.07),
+                new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, 44.42),
+                new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, 0.91));
+        Invoice invoice = new Invoice(1L, 1, "112/05/2024", 1,
+                null, null, null, null, null, invoiceItems);
+        Double expectedInvoiceNetAmount = 334.35 + 1372.18 + 893.62;
+        //WHEN
+        Double actualInvoiceNetAmount = calculator.calculateNetInvoiceAmount(invoice);
+        //THEN
+        assertThat(actualInvoiceNetAmount).isEqualTo(expectedInvoiceNetAmount);
+    }
+
+    @Test
+    void shouldCalculateCorrectInvoiceGrossAmount() {
+        //GIVEN
+        Set<InvoiceItem> invoiceItems = Set.of(
+                new InvoiceItem(1L, 1L, 1L, 15, 22.29, 0.08, 24.07),
+                new InvoiceItem(2L, 2L, 1L, 38, 36.11, 0.23, 44.42),
+                new InvoiceItem(3L, 3L, 1L, 982, 0.91, 0.00, 0.91));
+        Invoice invoice = new Invoice(1L, 1, "112/05/2024", 1,
+                null, null, null, null, null, invoiceItems);
+        Double expectedInvoiceGrossAmount = 24.07 * 15 + 44.42 * 38 + 0.91 * 982;
+        //WHEN
+        Double actualInvoiceGrossAmount = calculator.calculateGrossInvoiceAmount(invoice);
+        //THEN
+        assertThat(actualInvoiceGrossAmount).isEqualTo(expectedInvoiceGrossAmount);
     }
 
 
